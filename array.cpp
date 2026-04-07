c1fc2c3fe01ae4eff41d4ca11d663b6c57c1c51c
r4zkaaa <rzkiiahmd11@gmail.com>
2026-04-07 18:21:11 +0700

    Deklarasi array

--- array.cpp
+++ array.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
 using namespace std;    
+
+int main ()
+{
+    //Deklarasi array disertai inisialisasi
+    int dataku[5] = {10, 20, 30, 40, 50};
+    //deklrasi array tanpa inisialisasi
+    int datamu[5];
