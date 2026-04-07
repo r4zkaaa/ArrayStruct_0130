6ff1d27a306e8a2eaaf44e2b5ca3fd1dec32fe3d
r4zkaaa <rzkiiahmd11@gmail.com>
2026-04-07 20:16:33 +0700

    End commit

--- array.cpp
+++ array.cpp
@@ -45,3 +45,11 @@ int main ()
     cout<< endl;
 
       cout << "Tampil data array menggunakan for" << endl;
+       //menampilkan data dengan perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data ke-" << (i+1) << " : " << datamu [i] << endl;
+    }
+
+    
+}
